5a4184eb672c2667c34f0c9db91f11f8dbd27194
Sujitt Rameshkumar <rameshkumar.5@osu.edu>
2021-02-20 22:20:30 -0500

    created attributes map

--- main.py
+++ main.py
@@ -3,6 +3,29 @@ import numpy as np
 from DataReader import DataReader
 
 
+attributes = ['cap-shape',
+              'cap-surface',
+              'cap-color', 
+              'bruises?', 
+              'odor', 
+              'gill-attachment', 
+              'gill-spacing', 
+              'gill-size', 
+              'gill-color', 
+              'stalk-shape', 
+              'stalk-root', 
+              'stalk-surface-above-ring', 
+              'stalk-surface-below-ring', 
+              'stalk-color-above-ring', 
+              'stalk-color-below-ring', 
+              'veil-type', 
+              'veil-color', 
+              'ring-number', 
+              'ring-type', 
+              'spore-print-color', 
+              'population', 
+              'habitat'
+              ]
 
 
 def entropy(p_i):
@@ -61,13 +84,41 @@ pc_entropy = 0
 for p_i in probabilities:
     pc_entropy += entropy(p_i)
     
-
+print(pc_entropy)
 
 #Doesn't work, total values are all wrong. Need to account of coefficient total (out of 6000) and smaller total (out of total for that specific attribute i.e 2693 for x)
 first_attr_entropy = calculateColumnEntropy(df[1].value_counts().to_list())
 
 
-attr_dict = {}
 
-for data in df.iterrows():
-    print(data[1])
+# df.iloc[0,0] first row first column 'e'
+
+
+#this is a dictionary of dictionaries for each attribute. 
+#Will be parsing this map to find entropy values for each attribute
+all_attribute_dictionaries = {}
+
+
+
+def attributeDict(x):
+    attr_dict = {}
+    for i in range (6000):
+        row = df.iloc[i]
+        pc = row[0]
+        cap_shape = row[x]
+
+        curr_attr = pc + "_" + cap_shape
+        if (curr_attr in attr_dict):
+            # increment count of that attribute pair by 1
+            attr_dict[curr_attr] = attr_dict.get(curr_attr) + 1
+        else:
+            attr_dict[curr_attr] = 1  
+    return attr_dict
+
+# all_attribute_dictionaries[attributes[0]] = attributeDict(0)
+
+for i in range(1,23):
+    all_attribute_dictionaries[attributes[i-1]] = attributeDict(i)
+
+print(all_attribute_dictionaries)
+
